0c02e110d4e83a8645eec81c1d8d8fac7031887c
sonodima <62671493+sonodima@users.noreply.github.com>
2022-10-30 01:20:31 +0200

    added aobscan-cli formula

---
+++ Formula/aobscan.rb
@@ -0,0 +1,18 @@
+class Aobscan < Formula
+  desc "AOBscan CLI is a command-line utility for multi-threaded AOB memory scanning"
+  homepage "https://github.com/sonodima/aobscan-cli"
+  version "0.2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-aarch64-apple-darwin.tar.gz"
+    end
+    on_intel do
+      url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-apple-darwin.tar.gz"
+    end
+  end
+  
+  on_linux do
+    url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-unknown-linux-gnu.tar.gz"
+  end
+end
