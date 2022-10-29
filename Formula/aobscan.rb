7963acab2794adae3faaeb33fd984902115810f5
sonodima <62671493+sonodima@users.noreply.github.com>
2022-10-30 01:47:54 +0200

    added linux and intel hashes

--- Formula/aobscan.rb
+++ Formula/aobscan.rb
@@ -10,11 +10,13 @@ class Aobscan < Formula
     end
     on_intel do
       url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-apple-darwin.tar.gz"
+      sha256 "b9f898ad4b266804f5e26055ee3ad46a49a76e425d5779496489c3518c3007b8"
     end
   end
   
   on_linux do
     url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "f40ceba119069f4ce7ac0da3ef54bfa9f1035be166526517ec99a327792dc308"
   end
 
   def install
