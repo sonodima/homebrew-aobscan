28605bf25789340e516203019c4986c9a1c15ce2
sonodima <62671493+sonodima@users.noreply.github.com>
2022-11-14 10:46:54 +0100

    updated aobscan to v1.0.0

--- Formula/aobscan.rb
+++ Formula/aobscan.rb
@@ -1,22 +1,22 @@
 class Aobscan < Formula
   desc "AOBscan CLI is a command-line utility for multi-threaded AOB memory scanning"
   homepage "https://github.com/sonodima/aobscan-cli"
-  version "0.2.0"
+  version "1.0.0"
 
   on_macos do
     on_arm do
       url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-aarch64-apple-darwin.tar.gz"
-      sha256 "d3dc66dd8efe8951cd6100cc55954c9d0b51f1975004426a0326457a5b71336d"
+      sha256 "6f0981a93109b58b54e57938864bf0a5d070092730d9bde3e74f8534a9ba9ea7"
     end
     on_intel do
       url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-apple-darwin.tar.gz"
-      sha256 "b9f898ad4b266804f5e26055ee3ad46a49a76e425d5779496489c3518c3007b8"
+      sha256 "1cfd80559a82c0988f8d137926c92be4cd51fec651e55585674046b771ffb9f5"
     end
   end
   
   on_linux do
     url "https://github.com/sonodima/aobscan-cli/releases/latest/download/aobscan-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f40ceba119069f4ce7ac0da3ef54bfa9f1035be166526517ec99a327792dc308"
+    sha256 "efd3719efc0ff2326228d0b264e7253256c033142848091851ea11e51215a9e3"
   end
 
   def install
